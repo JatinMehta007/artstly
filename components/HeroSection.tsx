7c6b8aa5f5b4a42eccbbc7d67eca651dfd8830d7
Jatin_Mehta <mehtajatin045@gmail.com>
2025-06-24 12:06:49 +0530

    make mobile responsive

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -21,9 +21,9 @@ export const HeroSection=()=>{
      const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
     return (
         <>
-         <section className="px-6 py-16 text-center bg-gradient-to-br from-indigo-100 via-white to-white">
+         <section className="px-6  py-5 md:py-16 text-center bg-gradient-to-br from-indigo-100 via-white to-white">
                 <div
-              className="h-[800px] bg-black w-full  rounded-md  pt-40 overflow-clip"
+              className="h-[400px] md:h-[800px] bg-black w-full  rounded-md  pt-10 md:pt-40 overflow-clip"
               ref={ref}
             >      
             <GoogleGeminiEffect
